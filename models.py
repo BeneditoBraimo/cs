af17dad1b1da04517c8b45e7a4aa237546c62d5a
BeneditoBraimo <beneditobraimo@gmail.com>
2022-11-09 18:49:03 +0200

    added app database configurations

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import config
+
+app = Flask(__name__)
+
+# database configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
