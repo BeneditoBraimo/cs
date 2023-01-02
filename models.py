6dc7814e25bfc675aaa76bb95ec6dc3d39bea560
BeneditoBraimo <beneditobraimo@gmail.com>
2023-01-02 15:36:53 +0200

    formatted models.py file with black code formmater

--- models.py
+++ models.py
@@ -25,7 +25,8 @@ class Agent(db.Model):
     recommendations = db.relationship(
         "Agent", backref="recommendations", lazy=True, cascade="all, delete-orphan"
     )
-    
+
+
 class Province(db.Model):
     __tablename__ = "provinces"
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +34,8 @@ class Province(db.Model):
     locations = db.relationship(
         "Province", backref="locations", lazy=True, cascade="all, delete-orphan"
     )
+
+
 class Location(db.Model):
     __tablename__ = "locations"
     id = db.Column(db.Integer, primary_key=True)
@@ -48,7 +51,6 @@ class Location(db.Model):
     )
 
 
-
 class Occurrence(db.Model):
     __tablename__ = "occurrences"
     id = db.Column(db.Integer, primary_key=True)
@@ -59,12 +61,14 @@ class Occurrence(db.Model):
     agent_id = db.Column(db.Integer, db.ForeignKey(Agent.id))
     location_id = db.Column(db.Integer, db.ForeignKey(Location.id))
 
+
 class Recommendation(db.Model):
     __tablename__ = "recommendations"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     relevance = db.Column(db.String(), nullable=False)
     agent_id = db.Column(db.Integer, db.ForeignKey(Agent.id))
+
 
 class Event(db.Model):
     __tablename__ = "events"
