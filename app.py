4317a1f560e0122332b8a96cf2fb87647ca39460
BeneditoBraimo <beneditobraimo@gmail.com>
2023-01-02 16:27:37 +0200

    added index endpoint

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import request, render_template
 from flask_migrate import Migrate
 from flask_moment import Moment
 from models import app, db
@@ -7,3 +8,10 @@ moment = Moment(app)
 app.config.from_object(config)
 
 migrate = Migrate(app, db)
+
+
+# endpoints
+
+@app.route("/index")
+def index():
+    pass
