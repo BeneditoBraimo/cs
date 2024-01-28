f677f8bda5b142bcbce2475d7ad51904ef4a0aa6
BeneditoBraimo <beneditobraimo@gmail.com>
2024-01-28 10:20:41 +0200

    added OccurrenceForm class corresponding to the occurrence_form.html fields

--- forms.py
+++ forms.py
@@ -1,3 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+class OccurrenceForm(FlaskForm):
+    occurrence_type = StringField('Occurrence type')
+    occurrence_date = StringField('Occurrence data')
+    description = StringField('Description')
+    status = StringField('Status')
